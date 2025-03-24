2e2751f7be23997ded2dd0afe025cbfda94cc1ef
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-03-24 12:38:24 -0400

    Add CI workflow

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   description = "copilot";
 
   nixConfig = {
-    extra-substituters =
-      [ "https://nix-community.cachix.org" "https://numtide.cachix.org" ];
+    extra-substituters = [
+      "https://copilot.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+    ];
 
     extra-trusted-public-keys = [
+      "copilot.cachix.org-1:S3bdMN1HiDuXaJsh88/Tb0ZjkK3QPmYJRny9hViEQWk="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
@@ -26,7 +30,7 @@
 
       imports = with inputs; [ treefmt.flakeModule ];
 
-      perSystem = { config, pkgs, lib, system, ... }: {
+      perSystem = { config, pkgs, lib, ... }: {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs;
             [ cachix elixir just ] ++ [ config.treefmt.build.wrapper ]
@@ -47,33 +51,6 @@
             export PATH="$HEX_HOME/bin:$PATH"
           '';
         };
-
-        # devenv.shells.default = _: {
-        #   packages = with pkgs;
-        #     [ inputs.devenv.packages.${system}.default cachix elixir just ]
-        #     ++ [ config.treefmt.build.wrapper ]
-        #     ++ (builtins.attrValues config.treefmt.build.programs)
-        #     ++ lib.optionals stdenv.isLinux [ inotify-tools ]
-        #     ++ lib.optionals stdenv.isDarwin
-        #     (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
-
-        #   env = {
-        #     LANG = "en_US.UTF-8";
-        #     ERL_AFLAGS = "-kernel shell_history enabled";
-        #     MIX_ENV = "dev";
-        #   };
-
-        #   enterShell = ''
-        #     export MIX_HOME="$PWD/.elixir/mix"
-        #     export HEX_HOME="$PWD/.elixir/hex"
-
-        #     export PATH="$MIX_HOME/bin:$PATH"
-        #     export PATH="$HEX_HOME/bin:$PATH"
-        #   '';
-
-        #   # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
-        #   containers = lib.mkForce { };
-        # };
 
         treefmt.config = {
           projectRootFile = "flake.nix";
